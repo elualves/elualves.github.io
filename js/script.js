3f967633557b71e6191bc8f4994e0eb5775376d4
elualves <elualvess@gmail.com>
2020-02-14 17:21:15 -0300

    Conteúdo

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-script.js
+function efeito_no_icone_telefone() {
+	document.getElementById("icone_telefone").src = "../imagens/telefone_num.png"
+}
+function retorna_efeito_telefone() {
+	document.getElementById("icone_telefone").src = "../imagens/icone_telefone.png"
+}
+
+
+function efeito_no_icone_email() {
+	document.getElementById("icone_email").src = "../imagens/email_num.png"
+}
+function retorna_efeito_email() {
+	document.getElementById("icone_email").src = "../imagens/icone_email.png"
+}
+
+
+function efeito_no_icone_whatsapp() {
+	document.getElementById("icone_whatsapp").src = "../imagens/whatsapp_num.png"
+}
+function retorna_efeito_whatsapp() {
+	document.getElementById("icone_whatsapp").src = "../imagens/icone_whatsapp.png"
+}
