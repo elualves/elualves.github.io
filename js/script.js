be428d0531072dea1808a197909913de55d133b7
elualves <elualvess@gmail.com>
2020-03-21 11:27:23 -0300

    Conteúdo 2103202011:27

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 function efeito_no_icone_telefone() {
-	document.getElementById("icone_telefone").src = "../imagens/telefone_num.png"
+	document.getElementById("icone_telefone").src = "../../imagens/telefone_num.png"
 }
 function retorna_efeito_telefone() {
-	document.getElementById("icone_telefone").src = "../imagens/icone_telefone.png"
+	document.getElementById("icone_telefone").src = "../../imagens/icone_telefone.png"
 }
 
 
 function efeito_no_icone_email() {
-	document.getElementById("icone_email").src = "../imagens/email_num.png"
+	document.getElementById("icone_email").src = "../../imagens/email_num.png"
 }
 function retorna_efeito_email() {
-	document.getElementById("icone_email").src = "../imagens/icone_email.png"
+	document.getElementById("icone_email").src = "../../imagens/icone_email.png"
 }
 
 
 function efeito_no_icone_whatsapp() {
-	document.getElementById("icone_whatsapp").src = "../imagens/whatsapp_num.png"
+	document.getElementById("icone_whatsapp").src = "../../imagens/whatsapp_num.png"
 }
 function retorna_efeito_whatsapp() {
-	document.getElementById("icone_whatsapp").src = "../imagens/icone_whatsapp.png"
+	document.getElementById("icone_whatsapp").src = "../../imagens/icone_whatsapp.png"
 }
 
